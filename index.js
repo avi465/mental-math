c54cc26c088b49199326bf41cd4325904eeb6e19
Avinash <donavi917@gmail.com>
2021-08-10 01:40:38 +0530

    Add landing page, menu, quiz section

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ function operatorGenerator() {
 // o: if "n" is true then it take operator which we want such as "+", "-", "*", "/"
 // example: questionGenerator(2, 455, true, "*");
 
-
 // Question generator
 function questionGenerator(x, y, n, o) {
     let operator = [];
@@ -95,7 +94,6 @@ function questionGenerator(x, y, n, o) {
     data.correctOption.push(corr);
 }
 
-
 // Oject for storing data
 let data = {
     level: "",
@@ -106,11 +104,38 @@ let data = {
 }
 
 
+
+// Dom manipulations
 let score = 0;
 let percentScore = 0;
 let counter = -1;
 let chsnOptCounter = -1;
 
+//on loading function
+window.onload = load;
+
+function load() {
+    document.getElementById("landing").style.display = "initial";
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("quiz").style.display = "none";
+    document.getElementById("result").style.display = "none";
+}
+
+// landing Page
+function landingPage() {
+    document.getElementById("landing").style.display = "none";
+    document.getElementById("menu").style.display = "initial";
+    document.getElementById("quiz").style.display = "none";
+    document.getElementById("result").style.display = "none";
+}
+
+function start() {
+    document.getElementById("landing").style.display = "none";
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("quiz").style.display = "initial";
+    document.getElementById("result").style.display = "none";
+    render();
+}
 
 function render(clicked) {
     counter += 1;
@@ -121,12 +146,10 @@ function render(clicked) {
     document.getElementById("2").innerHTML = "(c). " + data.option[counter][2];
     document.getElementById("3").innerHTML = "(d). " + data.option[counter][3];
 
-
-
-    if (clicked) {
-        chsnOptCounter += 1;
-        data.choosenOption.push(clicked);
-    }
+    // if (clicked) {
+    //     chsnOptCounter += 1;
+    //     data.choosenOption.push(clicked);
+    // }
 
     // console logs
     console.log("Question:", (counter + 1), data.question[counter]);
@@ -135,47 +158,34 @@ function render(clicked) {
     console.log("correct options", data.correctOption);
     console.log("choosen option", data.choosenOption);
 
+    // if (counter === 9) {
+    //     for (let i = 0; i <= counter; i++) {
+    //         document.getElementById("q" + i).innerHTML = data.question[i];
+    //         document.getElementById(i + "0").innerHTML = data.option[i][0];
+    //         document.getElementById(i + "1").innerHTML = data.option[i][1];
+    //         document.getElementById(i + "2").innerHTML = data.option[i][2];
+    //         document.getElementById(i + "3").innerHTML = data.option[i][3];
 
-    if (counter === 9) {
-        for (let i = 0; i <= counter; i++) {
-            document.getElementById("q" + i).innerHTML = data.question[i];
-            document.getElementById(i + "0").innerHTML = data.option[i][0];
-            document.getElementById(i + "1").innerHTML = data.option[i][1];
-            document.getElementById(i + "2").innerHTML = data.option[i][2];
-            document.getElementById(i + "3").innerHTML = data.option[i][3];
+    //     }
+    // }
 
-        }
-    }
-
-
-    
-
-if (data.choosenOption.length == 9) {
-    for (let i = 0; i < 9; i++) {
-        if (data.option[i][data.correctOption[i]] == data.option[i][data.choosenOption[i]]) {
-            score += 10;
-            percentScore = (score/100)*100;
-            console.log("score: ",score);
-            console.log("percent: ", percentScore);
-        }
+// if (data.choosenOption.length == 9) {
+//     for (let i = 0; i < 9; i++) {
+//         if (data.option[i][data.correctOption[i]] == data.option[i][data.choosenOption[i]]) {
+//             score += 10;
+//             percentScore = (score/100)*100;
+//             console.log("score: ",score);
+//             console.log("percent: ", percentScore);
+//         }
         
-    }
-}
+//     }
+// }
 
-
-    if (counter > 8) {
-        counter = -1;
-        data.question = [];
-        data.option = [];
-        data.correctOption = [];
-    };
-
-    if (chsnOptCounter > 8) {
-        chsnOptCounter = -1;
-        data.choosenOption = [];
-    }
-
+    // if (counter > 8) {
+    //     counter = -1;
+    //     data.question = [];
+    //     data.option = [];
+    //     data.correctOption = [];
+    // };
 
 }
-
-
